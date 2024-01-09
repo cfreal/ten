b26aa2f771ca2047412370954af78ad9453681d2
cfreal <folcharles@gmail.com>
2024-01-09 17:08:31 +0100

    tf: String was meant to be an f-string

--- tenlib/transform/__main__.py
+++ tenlib/transform/__main__.py
@@ -32,7 +32,7 @@ def main(*transforms, python=False, keep_newline=False):
         instance = getattr(tf, module, None)
 
         if not instance:
-            return msg_error("Unknown module [red]{module}")
+            return msg_error(f"Unknown module [red]{module}")
 
         instance = getattr(instance, func, None)
 
